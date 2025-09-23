3684607e01ee4c906bde5e23848de57fc61421aa
Daniel Acevedo <daniel18acevedo@gmail.com>
2025-09-23 15:48:24 -0300

    Update security-group.tf

--- security-group.tf
+++ security-group.tf
@@ -5,14 +5,14 @@ resource "aws_security_group" "service" {
   tags        = merge(var.tags, { Name = "${var.name}-svc-sg" })
 }
 
-# Egress all (pull dependencies, call out)
+# Egress all
 resource "aws_vpc_security_group_egress_rule" "svc_all_egress" {
   security_group_id = aws_security_group.service.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
 
-# Ingress from ALB to container port (if exposed)
+# Ingress from ALB SG to container port (public via ALB)
 resource "aws_vpc_security_group_ingress_rule" "from_alb" {
   count                        = var.expose_via_alb && var.alb_security_group_id != null ? 1 : 0
   security_group_id            = aws_security_group.service.id
@@ -22,7 +22,7 @@ resource "aws_vpc_security_group_ingress_rule" "from_alb" {
   to_port                      = var.container_port
 }
 
-# Internal access: allow from selected SGs if not using ALB
+# Internal-only: allow from selected SGs
 resource "aws_vpc_security_group_ingress_rule" "from_internal_sgs" {
   for_each = var.expose_via_alb ? {} : toset(var.allowed_source_sg_ids)
   security_group_id            = aws_security_group.service.id
