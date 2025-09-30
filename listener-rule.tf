49c0fde6504c142302ed1615663d4e935016c5e9
Daniel Acevedo <daniel18acevedo@gmail.com>
2025-09-30 11:20:09 -0300

    Update listener-rule.tf

--- listener-rule.tf
+++ listener-rule.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_listener_rule" "https_host" {
-  count        = var.expose_via_alb && var.https_listener_arn != null && length(var.hostnames) > 0 ? 1 : 0
-  listener_arn = var.https_listener_arn
+  count        = var.expose_via_alb && local.effective_https_listener_arn != null && length(var.hostnames) > 0 ? 1 : 0
+  listener_arn = local.effective_https_listener_arn
   priority     = var.listener_rule_priority
 
   action {
