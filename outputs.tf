fff04ade1ee291a00bdc3d7f2ddfaec0545ec265
Daniel Acevedo <daniel18acevedo@gmail.com>
2025-09-30 11:10:56 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,3 @@
-output "service_name"        { value = aws_ecs_service.this.name }
-output "service_arn"         { value = aws_ecs_service.this.arn }
-output "task_definition_arn" { value = aws_ecs_task_definition.this.arn }
-output "service_sg_id"       { value = aws_security_group.service.id }
-output "log_group_name"      { value = aws_cloudwatch_log_group.this.name }
-
-output "target_group_arn" {
-  value       = try(aws_lb_target_group.this[0].arn, null)
-  description = "Target group ARN (if exposed)"
-}
-
-output "listener_rule_arn" {
-  value       = try(aws_lb_listener_rule.host_443[0].arn, null)
-  description = "Listener rule ARN on :443 (host-based)"
-}
+output "service_name"      { value = aws_ecs_service.this.name }
+output "service_sg_id"     { value = aws_security_group.svc.id }
+output "target_group_arn"  { value = aws_lb_target_group.this.arn }
