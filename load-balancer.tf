f08e5c84edaba14d6b2bae8494c021fef57bd3c5
Daniel Acevedo <daniel18acevedo@gmail.com>
2025-09-30 17:32:44 -0300

    Update load-balancer.tf

--- load-balancer.tf
+++ load-balancer.tf
@@ -15,7 +15,7 @@ resource "aws_lb_listener_rule" "host_443" {
 
   lifecycle {
     precondition {
-      condition     = var.https_listener_arn != null && length(var.hostnames) > 0 && var.listener_rule_priority != null
+      condition     = local.effective_https_listener_arn != null && length(var.hostnames) > 0 && var.listener_rule_priority != null
       error_message = "When expose_via_alb=true you must set https_listener_arn, hostnames and listener_rule_priority."
     }
   }
