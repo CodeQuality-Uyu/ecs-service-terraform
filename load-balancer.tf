6da83bba929a7d6b281d51bd135a12b35114d778
Daniel Acevedo <daniel18acevedo@gmail.com>
2025-09-30 12:22:50 -0300

    Remove aws_lb_target_group resource definition
    Removed the aws_lb_target_group resource definition from the load-balancer.tf file.

--- load-balancer.tf
+++ load-balancer.tf
@@ -1,26 +1,3 @@
-# Target Group (ip targets for Fargate)
-resource "aws_lb_target_group" "this" {
-  count       = var.expose_via_alb ? 1 : 0
-  name        = substr(replace("${var.name}-tg", ".", "-"), 0, 32)
-  port        = var.container_port
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "ip"
-
-  health_check {
-    enabled             = true
-    path                = var.health_path
-    healthy_threshold   = 2
-    unhealthy_threshold = 3
-    interval            = 30
-    timeout             = 5
-    matcher             = "200-399"
-  }
-
-  deregistration_delay = 15
-  tags                 = var.tags
-}
-
 # Host-based rule on shared HTTPS :443 listener
 resource "aws_lb_listener_rule" "host_443" {
   count        = var.expose_via_alb ? 1 : 0
