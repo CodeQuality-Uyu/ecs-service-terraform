535d6cf741be532971da739666d2ad39133d2f55
Daniel Acevedo <daniel18acevedo@gmail.com>
2025-09-30 11:10:34 -0300

    Update dns.tf

--- dns.tf
+++ dns.tf
@@ -1,6 +1,5 @@
-# Per-service Route53 A/ALIAS records for hostnames → ALB
-resource "aws_route53_record" "host_alias" {
-  for_each = var.expose_via_alb && var.create_dns_records ? toset(var.hostnames) : []
+resource "aws_route53_record" "alias" {
+  for_each = var.create_dns_records && var.route53_zone_id != null ? toset(var.hostnames) : toset([])
 
   zone_id = var.route53_zone_id
   name    = each.value
@@ -12,10 +11,5 @@ resource "aws_route53_record" "host_alias" {
     evaluate_target_health = false
   }
 
-  lifecycle {
-    precondition {
-      condition     = var.route53_zone_id != null && var.alb_dns_name != null && var.alb_zone_id != null
-      error_message = "To create DNS records set route53_zone_id, alb_dns_name, alb_zone_id."
-    }
-  }
+  allow_overwrite = true
 }
