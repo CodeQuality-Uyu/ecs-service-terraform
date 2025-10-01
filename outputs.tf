46d49b8b32ee69709ce618a8b6adb373559a7397
Daniel Acevedo <daniel18acevedo@gmail.com>
2025-09-30 21:19:16 -0300

    Add repository_url output to outputs.tf
    Add output for ECR repository URL with description.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "service_name"      { value = aws_ecs_service.this.name }
 output "service_sg_id"     { value = aws_security_group.svc.id }
 output "target_group_arn"  { value = aws_lb_target_group.this.arn }
+output "repository_url" {
+  value       = aws_ecr_repository.this.repository_url
+  description = "Full ECR repo URI"
+}
